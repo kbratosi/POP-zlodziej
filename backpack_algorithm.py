f77fbdd67b5d3abd5a78b4e00af9dea690a00bcc
Konrad Bratosiewicz <konrad.bratosiewicz.stud@pw.edu.pl>
2022-01-06 16:42:35 +0100

    refactor: reduced amount of copying

--- backpack_algorithm.py
+++ backpack_algorithm.py
@@ -3,34 +3,37 @@ import copy
 
 
 class Field:
-    def __init__(self):
-        self.score = 0
-        self.index_list = []
+    def __init__(self, score=0, index_list=[]):
+        self.score = score
+        self.index_list = index_list
 
     def __repr__(self):
         return str(self.score)
 
 
-A = [1, 2, 3, 4]
-V = [4, 3, 2, 1]
+A = [1, 2, 3, 4, 5, 7, 10, 2]
+V = [10, 8, 9, 4, 4, 13, 4, 9]
 
-W = 6
+W = 17
 
-P = [[Field() for _ in range(W + 1)] for _ in range(len(A) + 1)]
+P = [[Field() for _ in range(W + 1)]]
+for _ in range(len(A)):
+    P.append([Field()])
 
 for i in range(1, len(A) + 1):
     for j in range(1, W + 1):
         if A[i - 1] > j:
-            P[i][j] = copy.deepcopy(P[i - 1][j])
+            P[i].append(copy.deepcopy(P[i - 1][j]))
         else:
             skip_score = P[i - 1][j].score
             take_score = P[i - 1][j - A[i - 1]].score + V[i - 1]
             if take_score > skip_score:
-                P[i][j].score = take_score
-                P[i][j].index_list = copy.deepcopy(P[i - 1][j - A[i - 1]].index_list)
-                P[i][j].index_list.append(i - 1)
+                index_list = copy.deepcopy(P[i - 1][j - A[i - 1]].index_list)
+                index_list.append(i - 1)
+                P[i].append(Field(take_score, index_list))
             else:
-                P[i][j] = copy.deepcopy(P[i - 1][j])
+                P[i].append(copy.deepcopy(P[i - 1][j]))
+
 
 pprint(P)
-print("Answer: {}".format(P[len(A)][W].index_list))
+print("Answer: {}, score: {}".format(P[len(A)][W].index_list, P[len(A)][W].score))
