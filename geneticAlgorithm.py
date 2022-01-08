b591c830667f5a0d25758aee1bd01a2e49a13814
ProtegerPW <kobolte@gmail.com>
2022-01-08 17:43:08 +0100

    Solved minor naming problem

--- geneticAlgorithm.py
+++ geneticAlgorithm.py
@@ -6,29 +6,29 @@
 import sys, csv, getopt, json
 
 def main(argv):
-    generation_file = ''
+    characteristic_file = ''
     parameters_file = ''
     
     try:
-      opts, args = getopt.getopt(argv,"hg:p:",["generation=","parameters="])
+      opts, args = getopt.getopt(argv,"hc:p:",["characteristic=","parameters="])
     except getopt.GetoptError:
-      print("geneticAlgorithm.py -g <generation.csv> -p <parameters.json>")
+      print("geneticAlgorithm.py -g <characteristic.csv> -p <parameters.json>")
       sys.exit(2)
       
     for opt, arg in opts:
       if opt == '-h':
-         print("geneticAlgorithm.py -g <generation.csv> -p <parameters.json>")
+         print("geneticAlgorithm.py -g <characteristic.csv> -p <parameters.json>")
          sys.exit()
-      elif opt in ("-g", "--generation"):
-         generation_file = arg
+      elif opt in ("-c", "--characteristic"):
+         characteristic_file = arg
       elif opt in ("-p", "--parameters"):
          parameters_file = arg
     
-    if not (generation_file and parameters_file):
+    if not (characteristic_file and parameters_file):
         print("Both files are required")
         sys.exit()
         
-    return generation_file, parameters_file
+    return characteristic_file, parameters_file
 
 if __name__ == "__main__":
     csv_file_name, json_file_name = main(sys.argv[1:])
@@ -36,13 +36,9 @@ if __name__ == "__main__":
     csv_file = open(csv_file_name)
     csvreader = csv.reader(csv_file)
     
-    csv_rows = [row for row in csvreader]
-    # for row in csvreader: csv_rows.append(row)
-    
-    print(csv_rows)
+    csv_rows = [row for row in csvreader]    
     
     json_file = open(json_file_name)
     json_data = json.load(json_file)
-    
-    print(json_data['populations'])
-    print(json_data['mutation_probability'])
+
+    print(csv_rows)
